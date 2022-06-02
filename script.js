a6260e7991e5f7ed0165cfb269902c3b1c44f3b9
Muslim <m.altamirov@mail.ru>
2022-06-02 16:40:19 +0300

    start app

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const numberOffFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+	count: numberOffFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов', ''),
+		b = prompt('На сколько оцените его?', ''),
+		c = prompt('Один из последних просмотренных фиьмов?', ''),
+		d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
